ce129c8a3be3c5301ade47b0abb7bbf77ee7e3ff
Sara DM <ochuck.sdm@gmail.com>
2024-12-15 09:51:43 +0100

    Add routing for friends

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\FriendController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,9 +15,14 @@ Route::get('/games', function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile/notifications', [ProfileController::class, 'notifications'])->name('profile.notifications');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('games', GameController::class);
+    Route::get('/friends', [FriendController::class, 'index'])->name('friends.index');
+    Route::post('/friends', [FriendController::class, 'add'])->name('friends.add');
+    Route::post('/friends/accept', [FriendController::class, 'accept'])->name('friends.accept');
+    Route::delete('/friends', [FriendController::class, 'destroy'])->name('friends.destroy');
 });
 
 require __DIR__.'/auth.php';
